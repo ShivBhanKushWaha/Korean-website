69c5aa8589b5886dfac9c0d8626b1598700442ec
Shivbhan Kushwaha <108090221+ShivBhanKushWaha@users.noreply.github.com>
2024-07-19 01:38:53 +0530

    Update PlayList.ts
    Add path

--- src/app/AllDrama/DotOfSun/PlayList.ts
+++ src/app/AllDrama/DotOfSun/PlayList.ts
@@ -6,7 +6,7 @@ export const Episodes = [
         imageUrl: DotOfSun,
         verificationCode: '123456',
         videolink: 'https://1024terabox.com/s/1XtU1tTFSN7q5z8-SSrnSOw',
-        youtubeLink: '',
+        youtubeLink: 'https://youtu.be/ppgFkzQ10ow?si=Jofn2FQiGjkCtjbd',
         uploaded: true,
     },
     {
