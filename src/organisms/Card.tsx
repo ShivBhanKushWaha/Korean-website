fcbc0eb7b155bca810c27b053605ea11556baad8
unknown <shivbhankushwaha862003672001@gmail.com>
2024-07-20 01:33:21 +0530

    Add

--- src/organisms/Card.tsx
+++ src/organisms/Card.tsx
@@ -21,7 +21,7 @@ const Card: React.FC<CardProps> = ({ img, routes, title, desciption }) => {
   return (
     <div className='sm:w-full w-[280px]  max-w-md mx-auto my-4 cursor-pointer border border-gray-300 rounded-lg shadow-lg overflow-hidden hover:shadow-2xl transition duration-300 ease-in-out' onClick={handleClick}>
       <div className="relative h-40 overflow-hidden">
-        <Image src={img} alt={title} layout="fill" objectFit="cover" />
+        <Image src={img} alt={title} />
       </div>
       <div className="p-4">
         <p className="text-[18px] font-medium text-gray-800">{title}</p>
